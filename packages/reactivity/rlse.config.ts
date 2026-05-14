13b960f339524ec320e8556d14f6f73f8c18dbec
takuma-ru <takuma-ru@takumaru.dev>
2026-05-14 09:42:03 +0900

    fix: allow release level selection

--- packages/reactivity/rlse.config.ts
+++ packages/reactivity/rlse.config.ts
@@ -1,13 +1,18 @@
-import { defineConfig, presets } from "rlse.ts";
+import { defineConfig, presets, z } from "rlse.ts";
 
-export default defineConfig(
-  presets.npmRelease({
-    resolvePackage: { name: "@dathra/reactivity" },
-    calculateNextSemver: { level: "patch" },
-    runCommand: "pnpm build",
-    configureGitUser: {
-      name: "github-actions[bot]",
-      email: "41898282+github-actions[bot]@users.noreply.github.com",
-    },
+export default defineConfig({
+  args: z.object({
+    level: z.enum(["patch", "minor", "major", "preup"]).default("patch"),
+    pre: z.boolean().default(false),
   }),
-);
+  flow: ({ args }) =>
+    presets.npmRelease({
+      resolvePackage: { name: "@dathra/reactivity" },
+      calculateNextSemver: { level: args.level, pre: args.pre },
+      runCommand: "pnpm build",
+      configureGitUser: {
+        name: "github-actions[bot]",
+        email: "41898282+github-actions[bot]@users.noreply.github.com",
+      },
+    }),
+});
